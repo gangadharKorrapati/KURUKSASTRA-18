f01a2aadd13e9d4fa30d1dea5cd1075635d6a7ab
GANGADHAR KORRAPATI <119003080@sastra.ac.in>
2018-02-17 00:53:56 +0530

    init of ks development.

---
+++ js/ui.js
@@ -0,0 +1,32 @@
+impress().init();
+$(document).ready(function() { $('#preload').remove() });
+var api = impress();
+var intreval;
+api.init();
+document.addEventListener("impress:stepenter", function(event) {
+    $("#timeline_tracker").hide();
+    $("#timeline_tracker span").removeClass("year-highlight");
+    var data_type = $(".active").attr("data-type");
+    if (data_type == 'timeline') {
+        $("#timeline_tracker").show();
+        var data_year = $(".active").attr("data-year");
+        $("#nav-" + data_year).addClass("year-highlight");
+    }
+}, false);
+
+document.addEventListener("impress:stepenter", function(event) {
+    var data_id = $(".active").attr("id");
+    if (data_id == 'about') {
+        $("li").removeClass("menu_highlight");
+        $("#menu-about").addClass("menu_highlight");
+    } else if (data_id == 'home') {
+        $("li").removeClass("menu_highlight");
+        $("#menu-home").addClass("menu_highlight");
+    } else if (data_id == 'timeline-start') {
+        $("li").removeClass("menu_highlight");
+        $("#menu-timeline").addClass("menu_highlight");
+    } else if (data_id == 'sponsors') {
+        $("li").removeClass("menu_highlight");
+        $("#menu-sponsors").addClass("menu_highlight");
+    }
+}, false);
