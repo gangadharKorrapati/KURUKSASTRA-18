964ee260838ca97359c82c6ce3d1a97405c7abaa
harishpydimarri29 <harishpydimarri29@gmail.com>
2018-03-03 14:21:58 +0530

    ui.js is modified

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	setTimeout(remove, 5000);
 	$('#site').css("visibility", "visible");
+    $(".button-collapse").sideNav();
 });
 function remove(){
 	$('#preload').remove();
