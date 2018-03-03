da40cb3f9cb712b2f8d82597cf89bac57997bf43
GANGADHAR KORRAPATI <119003080@sastra.ac.in>
2018-03-03 21:44:28 +0530

    about completed

---
+++ dashboard.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>KS 18  |  Home</title>
+    <meta name="description" content="KS'18 The twelfth edition of Kuruksastra. March 23,24,25. Will surpass all previous installments. Visit for one of a kind experience. Three days of crazy fun.A sublime melange of style and substance, glamour and colour." />
+    <meta name="keywords" content="Kuruksastra , KS Upahaar , Sastra Deemed University cultural fest" />
+    <meta name="robots" content="index,follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="manifest" href="favicons/site.webmanifest">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#2b5797">
+    <link rel="shortcut icon" href="favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-config" content="favicons/browserconfig.xml">
+    <meta name="theme-color" content="#2b5797">
+    <link href="css/impress.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!--
+    <div id="preload-div">
+        <img id="preload" src="img/ks_animated.svg" alt="preloader ks">
+    </div>-->
+    <div id="site">
+    <nav class="blue accent-3">
+        <div class="nav-wrapper container">
+          <a href="index.html" class="brand-logo">Kuruksastra</a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            
+          <ul class="right hide-on-med-and-down">
+            <li><a href="events.html">Events</a></li>
+            <li><a href="sponsors.html">Sponsors</a></li>
+            <li><a href="about.html">About</a></li>  
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+
+          <ul class="side-nav center" id="mobile-demo">
+            <br>
+            <img src="img/user.png" style="width: 100px;height: 100px;border-radius:50%;">
+            <li><hr></li>
+            <li><a href="myprofile.php">Profile</a></li>
+            <li><a href="myevents.php">My Events</a></li>
+            <li><hr></li>
+            <li><a href="events.html">Events</a></li>
+            <li><a href="sponsors.html">Sponsors</a></li>
+            <li><a href="about.html">About</a></li>  
+            <li><a href="contact.html">Contact</a></li>
+            <li><hr></li>
+            <li><a class="btn" href="logout.php">Logout</a></li>  
+          </ul>
+        </div>
+  </nav>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col l3 center hide-on-med-and-down" style="border-right:1px solid grey">
+            <br>
+            <img src="img/user.png" style="width: 150px;height: 150px;border-radius:50%;">
+            <br><br><br>
+                <ul>
+                    <li><h5><a class="btn blue accent-3" href="myprofile.php">Profile</a></h5></li>
+                    <li><h5><a class="btn blue accent-3" href="myevents.php">My Events</a></h5></li>
+                    <li><h5><a class="btn blue accent-3" href="logout.php">Logout</a></h5></li>
+                </ul>
+                <br><br><br><br><br><br><br><br>
+        </div>
+        <div id="space" class="col s12 l9 center">
+            <!---->
+            <br>    
+            <h4>You haven't registerd to any event</h4>
+            <br>
+            <h5>Visit our Events and register</h5>
+            <br>
+            <a class="btn blue accent-3 pulse" href="events.html">Events</a>
+            
+        </div>
+    </div>
+    
+    <footer class="page-footer blue lighten-1">
+          <div>
+            <div class="row">
+                <div class="col l2 m2 s3 center">
+                    <img src="img/footer_ks.svg" style="width:100px;height:100px">
+                </div>
+              <div class="col l2 m3 s9 center">
+                <ul>
+                  <li><br></li>
+                  <li><a class="grey-text text-lighten-3" href="events.html">Events</a></li>
+                  <li><a class="grey-text text-lighten-3" href="sponsors.html">Sponsors</a></li>
+                  <li><a class="grey-text text-lighten-3" href="contact.html">Contact</a></li>
+                  <li><a class="grey-text text-lighten-3" href="about.html">About</a></li>
+                </ul>
+              </div>
+                
+                <div class="col l4 m7 s12 center">
+                <h5 class="white-text">ESPRIT  D'AVENTURE</h5>
+                <p class="grey-text text-lighten-4">
+                    Kuruksastra, marching towards a glorious 12th edition this year, presents to you stunning Pro-Nites, brilliant workshops and unlimited fun on the 23rd,24th,25th of March!    
+                </p>
+              </div>
+                <div class="col l4 m12 s12 center">
+                    <br>
+                    <a href="https://www.youtube.com/watch?v=iC1NiALnX3Y" target="_blank"><i class="fa fa-youtube-play" style="font-size:32px;color:red"></i></a>&nbsp;&nbsp;
+                    
+                    <a href="https://www.facebook.com/kssastra" target="_blank"><i class="fa fa-facebook-square" style="font-size:32px;color:blue"></i></a>&nbsp;&nbsp;
+                    
+                    <a href="https://www.instagram.com/kuruksastra" target="_blank"><i class="fa fa-instagram" style="font-size:32px;color:pink"></i></a>&nbsp;&nbsp;
+                    
+                    <a href="https://www.twitter.com/kssastra" target="_blank"><i class="fa fa-twitter-square" style="font-size:32px;color:skyblue"></i></a>
+                
+              </div>
+                
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2018 Copyright Kuruksastra by 300dpi
+            <a class="grey-text text-lighten-4 right" href="http://www.300dpi.xyz">About Us</a>
+            </div>
+          </div>
+        </footer>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/ui.js"></script>
+    
+</body>
+
+</html>
--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
 	$('#site').css("visibility", "visible");
     $(".button-collapse").sideNav();
 });
+function openmodel(modelid){
+	$(modelid).addClass("open");
+}
+function exitmodel(modelid){
+		$(modelid).removeClass("open");
+
+}
 function remove(){
 	$('#preload').remove();
 }
+  $('.modal').modal({
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        
+      },
+      complete: function() { 
+
+      } // Callback for Modal close
+    }
+  );
