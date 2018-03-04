a2e63c9719ea2abeeafe94f6f7542b6fb8d5cd20
GANGADHAR KORRAPATI <119003080@sastra.ac.in>
2018-03-05 00:27:35 +0530

    kkok

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>KS 18 | Home</title>
+    <title>KS 18  |  Home</title>
     <meta name="description" content="KS'18 The twelfth edition of Kuruksastra. March 23,24,25. Will surpass all previous installments. Visit for one of a kind experience. Three days of crazy fun.A sublime melange of style and substance, glamour and colour." />
     <meta name="keywords" content="Kuruksastra , KS Upahaar , Sastra Deemed University cultural fest" />
     <meta name="robots" content="index,follow" />
@@ -22,6 +22,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/materialize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 
 <body>
@@ -29,30 +30,62 @@
     <div id="preload-div">
         <img id="preload" src="img/ks_animated.svg" alt="preloader ks">
     </div>-->
-
     <div id="site">
-        <nav class=" red blue accent-3">
-            <div class="nav-wrapper container ">
-                <a href="index.html" class="brand-logo"><img src="img/kssnav.svg" style=" max-height: 64px; padding: 15px;"></a> <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="events.html">Events</a></li>
-                    <li><a href="sponsors.html">Sponsors</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-                <ul class="side-nav" id="mobile-demo">
-                    <li><a href="events.html">Events</a></li>
-                    <li><a href="sponsors.html">Sponsors</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+    <nav class="transparent">
+    <div class="nav-wrapper container">
+      <!--<a href="#!" class="brand-logo"><img src="img/ksnavwhite.svg" style="height:50px;width:175px;margin-top:6%"></a>-->
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="events.php">Events</a></li>
+        <li><a href="sponsors.php">Sponsors</a></li>
+        <li><a href="about.php">About</a></li>  
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+        <a href="#" class="brand-logo center"><img src="img/ksnavwhite.svg" style="height:50px;width:175px;margin-top:6%"></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a class="btn transparent" href="login.php">Login</a></li>
+            <li><a class="btn transparent" href="signup.php">SignUp</a></li>
+        </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="events.php">Events</a></li>
+        <li><a href="sponsors.php">Sponsors</a></li>
+        <li><a href="about.php">About</a></li>  
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
     </div>
     <div id="bg">
-    	
+    <br>
+    <br>
+    <br>
+        <div class="row center">
+            <div class="col s12 m12 l12 center">
+            <img class="" src="img/ksnavwhite.svg" style="height:60%px;width:65%;margin-top:2%">
+            <br>
+            <h4 class="white-text">2018 March 23rd 24th 25th</h4>
+            <br>
+            <a class="btn transparent" href="login.php">Login</a>&nbsp;&nbsp;
+            <a class="btn transparent" href="signup.php">Signup</a>&nbsp;&nbsp;
+            <a class="btn transparent" href="login.php">Campus Ambassodor</a>&nbsp;&nbsp;
+            </div>
+        </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     </div>
-    <footer class="page-footer blue lighten-1">
+    <footer class="page-footer transparent">
         <div>
             <div class="row">
                 <div class="col l2 m2 s3 center">
@@ -91,9 +124,12 @@
             </div>
         </div>
     </footer>
+    </div>
     <script src="js/jquery.min.js"></script>
     <script src="js/materialize.min.js"></script>
+    <script src="js/scrollreveal.min.js"></script>
     <script src="js/ui.js"></script>
+    
 </body>
 
 </html>
